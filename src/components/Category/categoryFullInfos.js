c12e51577516b6712f2faa8656de52a4b756b2c1
esima1 <mokemebratu@gmail.com>
2025-01-04 19:46:09 -0600

    ac2

--- src/components/Category/categoryFullInfos.js
+++ src/components/Category/categoryFullInfos.js
@@ -1,24 +1,26 @@
 export const categoryInfos = [
   {
     name: "jewelery",
-    title: "Solid Gold Petite",
-    imgLink: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+    title: "Necklaces",
+    imgLink:
+      "https://plus.unsplash.com/premium_photo-1674255466849-b23fc5f5d3eb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8amV3ZWxlcnl8ZW58MHx8MHx8fDA%3D",
   },
   {
     name: "men's clothing",
-    title: "Mens T-Shirts ",
+    title: "Mens Shirts ",
     imgLink:
-      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+      "https://images.unsplash.com/photo-1578238591955-f360d5eda965?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bWVuJ3MlMjBjbG90aGluZ3xlbnwwfHwwfHx8MA%3D%3D",
   },
   {
     name: "electronics",
-    title:
-      "Samsung 49-Inch Gaming Monitor",
-    imgLink: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
+    title: "CPU",
+    imgLink:
+      "https://images.unsplash.com/photo-1591799265444-d66432b91588?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3Ryb25pY3N8ZW58MHx8MHx8fDA%3D",
   },
   {
     name: "women's clothing",
-    title: "Rain Jacket Women",
-    imgLink: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
+    title: "Women Fashion",
+    imgLink:
+      "https://images.unsplash.com/photo-1608169941368-fd7ac986ac3d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
